5cb8e7536e63eaf9292c78db3e576dcb89b8dcdb
Caki1235813 <carna.djokovic@novelic.com>
2025-09-30 21:36:04 +0200

    Add new line to Hello world

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,8 @@ fn main() {
     println!("Čarna");
     println!("Rust je moj omiljeni programski jezik");
     println!("Ucim Rust zato sto se moj brat zove Rastko");
+    additional_line();
+}
+fn additional_line(){
+    println!("Ovo je dodatna linija koda");
 }
